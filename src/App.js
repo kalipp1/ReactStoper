73d163a306543e4f52bf3f65b478568bde6e3ace
Maks Birecki <maksymilian.birecki@gmail.com>
2024-09-10 22:52:54 +0200

    InitProject

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import Container from "./components/Container/Container";
+import Time from "./components/Time/Time";
+
+const App = () => {
+  return (
+    <div>
+    <Time />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Time/Time.js
@@ -0,0 +1,9 @@
+const Time = () => {
+    return (
+        <div>
+            00:00:00:0
+        </div>
+    );
+};
+
+export default Time;
